5f664002346d901a8e879ac405465bec8bee4c43
Population Codes <population.codes@gmail.com>
2025-06-24 17:45:58 +0300

    Fixed linter comments

--- ensembles/plot.py
+++ ensembles/plot.py
@@ -1,16 +1,17 @@
 import math
 
 import numpy as np
-import matplotlib as plt
 import matplotlib.patches as plt_patch
 
-def text(cx, x : float, y : float, message : str, **kwargs):
+
+def text(cx, x: float, y: float, message: str, **kwargs):
     xl = cx.get_xlim()
     yl = cx.get_ylim()
     cx.text(xl[0]*(1-x)+xl[1]*x, yl[0]*(1-y)+yl[1]*y, message, **kwargs)
 
+
 def show_real_spectrum(cx, z, only_positive=False):
-    assert np.all(np.abs(np.imag(z))<1e-10)
+    assert np.all(np.abs(np.imag(z)) < 1e-10)
     m = 1.1*np.max(np.abs(z))
 
     cx.hist(np.real(z), density=True, color='C7')
@@ -23,6 +24,7 @@ def show_real_spectrum(cx, z, only_positive=False):
     cx.spines["right"].set_visible(False)
     cx.spines["top"].set_visible(False)
 
+
 # Show an eigenvalue spectrum
 def show_complex_spectrum(cx, z, radius=None, center=[0, 0], rotation=1,
                           show_center=False, outlier=None, equal_aspect=True):
@@ -30,7 +32,7 @@ def show_complex_spectrum(cx, z, radius=None, center=[0, 0], rotation=1,
         angle = 0
     else:
         rotation /= np.abs(rotation)
-        angle = np.arctan2(np.imag(rotation), np.real(rotation))*180/math.pi
+        angle = math.atan2(np.imag(rotation), np.real(rotation))*180/math.pi
     mx1 = np.max(np.real(z))
     mx0 = np.min(np.real(z))
     my1 = np.max(np.imag(z))
@@ -40,19 +42,19 @@ def show_complex_spectrum(cx, z, radius=None, center=[0, 0], rotation=1,
         mx0 = np.minimum(mx0, center[0]-radius[0])
         my1 = np.maximum(my1, center[1]+radius[1])
         my0 = np.minimum(my0, center[1]-radius[1])
-    
+
     if outlier is not None:
         cx.plot(outlier[0], outlier[1], 'x', color='C8', markersize=20)
         mx0 = np.minimum(mx0, outlier[0])
         mx1 = np.maximum(mx1, outlier[0])
         my0 = np.minimum(my0, outlier[1])
         my1 = np.maximum(my1, outlier[1])
-    
+
     cx.scatter(np.real(z), np.imag(z), color='C7', edgecolor='w')
 
     if radius is not None:
         ellipsis = plt_patch.Ellipse(center, 2*radius[0], 2*radius[1],
-                                     color='C1', fill=None, angle=angle, 
+                                     color='C1', fill=None, angle=angle,
                                      linewidth=1.5)
         cx.add_patch(ellipsis)
 
